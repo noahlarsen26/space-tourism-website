d2e8a55d79d99d483e21eaf9e446ddfffc829cbd
Noah Larsen <noahlarsen24@gmail.com>
2023-01-30 19:18:53 -0600

    added text and button to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,24 @@
 function Home() {
-  return <h1>home</h1>;
+  return (
+    <div className="hero-container">
+      <article>
+        <h3>so, you want to travel to</h3>
+        <h2>space</h2>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </article>
+      <div className="circle-container">
+        <button className="circle">
+          <h5>explore</h5>
+        </button>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
