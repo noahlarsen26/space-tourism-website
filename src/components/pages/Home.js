f7289e8d13690356f65bd93824b28e001493a79c
Noah Larsen <noahlarsen24@gmail.com>
2023-01-30 19:39:21 -0600

    updated background

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,23 @@
 function Home() {
   return (
-    <div className="hero-container">
-      <article>
-        <h3>so, you want to travel to</h3>
-        <h2>space</h2>
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
-      </article>
-      <div className="circle-container">
-        <button className="circle">
-          <h5>explore</h5>
-        </button>
-        <div></div>
+    <div className="bg-1">
+      <div className="hero-container">
+        <article>
+          <h3>so, you want to travel to</h3>
+          <h2>space</h2>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </article>
+        <div className="circle-container">
+          <button className="circle">
+            <h5>explore</h5>
+          </button>
+          <div></div>
+        </div>
       </div>
     </div>
   );
