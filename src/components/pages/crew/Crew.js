2e8222dbbe44e30d1973ecf6183f693b2e7a4de9
Noah Larsen <noahlarsen24@gmail.com>
2023-02-15 18:57:10 -0600

    basic layout of crew page

--- src/components/pages/crew/Crew.js
+++ src/components/pages/crew/Crew.js
@@ -1,5 +1,37 @@
+import crewImg from "../../../assets/crew/image-douglas-hurley.png";
+
 function Crew() {
-  return <h1>Crew</h1>;
+  return (
+    <div className="bg-3">
+      <div className="crew">
+        <div className="heading-container">
+          <h1>
+            <span>02</span>meet your crew
+          </h1>
+        </div>
+        <div className="crew-container">
+          <div className="crew-member-info">
+            <h3>commander</h3>
+            <h2>douglas hurley</h2>
+            <p>
+              Douglas Gerald Hurley is an American engineer, former Marine Corps
+              pilot and former NASA astronaut. He launched into space for the
+              third time as commander of Crew Dragon Demo-2.
+            </p>
+            <div className="dots">
+              <button className="dot"></button>
+              <button className="dot"></button>
+              <button className="dot"></button>
+              <button className="dot"></button>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src={crewImg} alt="crew-member-image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Crew;
